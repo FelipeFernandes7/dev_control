17921020ad8006ee33b54be38025e3f464119c7c
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-11 14:26:47 -0300

    add props card customer

--- src/app/dashboard/customer/components/card.tsx
+++ src/app/dashboard/customer/components/card.tsx
@@ -1,14 +1,20 @@
-export function Card() {
+type Customer = {
+  name: string;
+  email: string;
+  phone: string;
+};
+
+export function Card({ name, email, phone }: Customer) {
   return (
     <article className="w-full md:max-w-[400px] flex flex-col bg-neutral-900 border border-neutral-800 rounded-2xl p-4 gap-2 hover:scale-105 duration-300 transition-all">
       <p className="text-sm">
-        <strong>Nome:</strong> ratazana
+        <strong>Nome:</strong> {name}
       </p>
       <p className="text-sm">
-        <strong>Email:</strong> example@gmail.com
+        <strong>Email:</strong> {email}
       </p>
       <p className="text-sm">
-        <strong>Telefone:</strong> (16)993044-9090
+        <strong>Telefone:</strong> {phone}
       </p>
       <button className="text-white h-8 px-4 mt-2 text-xs font-bold rounded-xl bg-indigo-500 self-start">
         Excluir
