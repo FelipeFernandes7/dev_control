5d4e6d6294d9b8b46ff841f6625cd94bee8d1dc1
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-11 15:26:05 -0300

    create delete route

--- src/app/api/customer/route.ts
+++ src/app/api/customer/route.ts
@@ -3,6 +3,57 @@ import { getServerSession } from "next-auth";
 import { NextResponse } from "next/server";
 import prismaClient from "@/lib/prisma";
 
+export async function DELETE(req: Request) {
+  const session = await getServerSession(authOptions);
+  const { searchParams } = new URL(req.url);
+  const userId = searchParams.get("id");
+
+  if (!session || !session.user) {
+    return NextResponse.json(
+      { error: "Not Authorized" },
+      {
+        status: 401,
+      },
+    );
+  }
+  if (!userId) {
+    return NextResponse.json(
+      { error: "Customer ID is required" },
+      {
+        status: 400,
+      },
+    );
+  }
+
+  const findTickets = await prismaClient.ticket.findFirst({
+    where: {
+      customerId: userId,
+    },
+  });
+  if (findTickets) {
+    return NextResponse.json(
+      {
+        error:
+          "Cliente possui tickets abertos, exclua todos os tickets antes de excluir o cliente.",
+      },
+      {
+        status: 400,
+      },
+    );
+  }
+  try {
+    await prismaClient.customer.delete({ where: { id: userId } });
+    return NextResponse.json({ message: "Cliente excluído com sucesso!" });
+  } catch (error) {
+    return NextResponse.json(
+      { error: "Failed to delete customer" },
+      {
+        status: 400,
+      },
+    );
+  }
+}
+
 export async function POST(req: Request) {
   const { name, email, phone, address } = await req.json();
   const session = await getServerSession(authOptions);
