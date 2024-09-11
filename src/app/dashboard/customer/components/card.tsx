691b01fc1fe553fee730cd1a177e2e1706c9de44
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-11 15:26:46 -0300

    refactored component

--- src/app/dashboard/customer/components/card.tsx
+++ src/app/dashboard/customer/components/card.tsx
@@ -1,22 +1,31 @@
-type Customer = {
-  name: string;
-  email: string;
-  phone: string;
-};
+"use client";
+import { deleteCustomer } from "@/services/customer";
+import { Customer } from "@/utils/customer.type";
+import { useRouter } from "next/navigation";
 
-export function Card({ name, email, phone }: Customer) {
+export function Card({ customer }: { customer: Customer }) {
+  const router = useRouter();
+
+  async function onDelete() {
+    await deleteCustomer(customer.id);
+    router.refresh();
+  }
+
   return (
     <article className="w-full md:max-w-[400px] flex flex-col bg-neutral-900 border border-neutral-800 rounded-2xl p-4 gap-2 hover:scale-105 duration-300 transition-all">
       <p className="text-sm">
-        <strong>Nome:</strong> {name}
+        <strong>Nome:</strong> {customer.name}
       </p>
       <p className="text-sm">
-        <strong>Email:</strong> {email}
+        <strong>Email:</strong> {customer.email}
       </p>
       <p className="text-sm">
-        <strong>Telefone:</strong> {phone}
+        <strong>Telefone:</strong> {customer.phone}
       </p>
-      <button className="text-white h-8 px-4 mt-2 text-xs font-bold rounded-xl bg-indigo-500 self-start">
+      <button
+        onClick={onDelete}
+        className="text-white h-8 px-4 mt-2 text-xs font-bold rounded-xl bg-indigo-500 self-start"
+      >
         Excluir
       </button>
     </article>
