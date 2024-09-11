841d99cebc7c545da131076d0d153c08c2e051be
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-11 14:31:06 -0300

    remove import

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { signIn, signOut, useSession } from "next-auth/react";
-import { Loader, LockKeyhole, LogOut, User } from "lucide-react";
+import { Loader, LogOut, User } from "lucide-react";
 
 import Link from "next/link";
 
