e4b9099d8bf6d264dbacdd61bcb51ff16dab7a28
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-11 14:27:59 -0300

    adjust customer route, add toast in server status

---
+++ src/services/customer.ts
@@ -0,0 +1,31 @@
+import { api } from "@/lib/api";
+import toast from "react-hot-toast";
+
+type Customer = {
+  name: string;
+  email: string;
+  phone: string;
+  address?: string;
+};
+
+export async function createCustomer({
+  name,
+  email,
+  phone,
+  address,
+}: Customer) {
+  const response = await api.post("/api/customer", {
+    name,
+    email,
+    phone,
+    address,
+  });
+  try {
+    toast.success(response.data.message);
+  } catch (error) {
+    console.error(error);
+    toast.error(response.data.message);
+  }
+
+  return response.data;
+}
