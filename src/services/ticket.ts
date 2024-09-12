5c1ea586e4588ead59798083cdd754197fb493b1
Felipe Fernandes <felipekurosaki123@gmail.com>
2024-09-12 13:12:18 -0300

    create ticket service api to axios

---
+++ src/services/ticket.ts
@@ -0,0 +1,16 @@
+import { api } from "@/lib/api";
+import toast from "react-hot-toast";
+
+export async function changeStatus(id: string) {
+  const response = await api.patch("api/ticket/", {
+    id: id,
+  });
+  try {
+    toast.success(response.data.message);
+  } catch (error) {
+    console.error(error);
+    toast.error(response.data.error);
+  }
+
+  return response.data;
+}
